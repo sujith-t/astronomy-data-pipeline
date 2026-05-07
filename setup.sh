e267d382c283798343f861344861e278559c67f5
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2026-05-07 15:10:15 +0530

    adding virtual environment, python package installation and setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# activate virtual environment and install dependancies
+# install all prerequisite tools
+sudo dnf update
+sudo dnf install python -y
+sudo dnf install git -y
+sudo yum install python3-pip -y
 
-source .venv/bin/activate
+# clone the repo
+cd /var
+git clone https://github.com/sujith-t/astronomy-data-pipeline.git
+
+# create a virtual environment and install the requirements
+cd astronomy-data-pipeline
+python -m venv astro
+source astro/bin/activate
 pip install -r pkg_dependancy.txt
