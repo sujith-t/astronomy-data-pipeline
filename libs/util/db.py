d57eb94c6fa1ccf377c6f9c61e26c29783c1c3df
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2026-05-08 20:57:21 +0530

    adding db utility

---
+++ libs/util/db.py
@@ -0,0 +1,60 @@
+import mysql.connector
+
+#from dotenv import load_dotenv
+
+#load_dotenv("../../.env")
+
+class MySQLUtil:
+
+    connection = None
+
+    def __init__(self, op_system):
+        self.query = list()
+        self.params = list()
+
+        if MySQLUtil.connection is None:
+            MySQLUtil.connection = mysql.connector.connect(host=op_system.getenv("DB_HOST"), user=op_system.getenv("DB_USER"), password=op_system.getenv("DB_PASSWORD"), database=op_system.getenv("DB_NAME"))
+
+    def execute(self, sql:str, params=None, commit=False):
+        self.query.append(sql)
+        self.params.append(params)
+
+        if commit:
+            cursor = MySQLUtil.connection.cursor()
+            for q, p in zip(self.query, self.params):
+                if p is None:
+                    cursor.execute(q)
+                else:
+                    cursor.execute(q, p)
+
+            MySQLUtil.connection.commit()
+            self.query.clear()
+            self.params.clear()
+            cursor.close()
+
+    def fetch_all(self, sql:str, params=None):
+        cursor = DbUtil.connection.cursor()
+        if params is None:
+            cursor.execute(sql)
+        else:
+            cursor.execute(sql, params)
+
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
+
+    def fetch_one(self, sql:str, params=None):
+        cursor = MySQLUtil.connection.cursor()
+        if params is None:
+            cursor.execute(sql)
+        else:
+            cursor.execute(sql, params)
+
+        row = cursor.fetchone()
+        cursor.close()
+        return row
+
+    def __close__(self):
+        if MySQLUtil.connection is not None:
+            MySQLUtil.connection.close()
+            MySQLUtil.connection = None
